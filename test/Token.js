b9dc81323182098ccef11345ff8ccb2e34424822
pakornsak.s <pakornsak.supamongkonchaikul@agoda.com>
2022-06-21 16:17:35 +0700

    revise test case

--- test/Token.js
+++ test/Token.js
@@ -2,31 +2,60 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Token contract", function () {
-  it("Deployment should assign total supply of tokens to the owner", async () => {
-    const [owner] = await ethers.getSigners();
-    const Token = await ethers.getContractFactory("Token");
-    const hardhatToken = await Token.deploy();
+describe("Token contract", () => {
+  // A common pattern is to declare some variables, and assign them in the
+  // `before` and `beforeEach` callbacks.
+  let Token;
+  let hardhatToken;
+  let owner;
+  let addr1;
+  let addr2;
+  let addrs;
 
-    const ownerBalance = await hardhatToken.balanceOf(owner.address);
-    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+  beforeEach(async () => {
+    // Get the ContractFactory and Signers here.
+    Token = await ethers.getContractFactory("Token");
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+    // To deploy our contract, we just have to call Token.deploy() and await
+    // for it to be deployed(), which happens once its transaction has been
+    // mined.
+    hardhatToken = await Token.deploy();
   });
-});
 
-describe("Transactions", function () {
-  it("Should transfer tokens between accounts", async function () {
-    const [owner, addr1, addr2] = await ethers.getSigners();
-    const Token = await ethers.getContractFactory("Token");
-    const hardhatToken = await Token.deploy();
+  describe("Deployment", () => {
+    it("should assign total supply of tokens to the owner", async () => {
+      const ownerBalance = await hardhatToken.balanceOf(owner.address);
+      expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+    });
+  });
 
-    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(0);
+  describe("Transactions", () => {
+    it("Should fail if sender doesn’t have enough tokens", async function () {
+      const initialOwnerBalance = await hardhatToken.balanceOf(owner.address);
 
-    await hardhatToken.transfer(addr1.address, 50);
-    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
-    expect(await hardhatToken.balanceOf(addr2.address)).to.equal(0);
+      // Try to send 1 token from addr1 (0 tokens) to owner (1000000 tokens).
+      // `require` will evaluate false and revert the transaction.
+      await expect(
+        hardhatToken.connect(addr1).transfer(owner.address, 1)
+      ).to.be.revertedWith("Not enough tokens");
 
-    await hardhatToken.connect(addr1).transfer(addr2.address, 50);
-    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(0);
-    expect(await hardhatToken.balanceOf(addr2.address)).to.equal(50);
+      // Owner balance shouldn't have changed.
+      expect(await hardhatToken.balanceOf(owner.address)).to.equal(
+        initialOwnerBalance
+      );
+    });
+
+    it("Should transfer tokens between accounts", async function () {
+      expect(await hardhatToken.balanceOf(addr1.address)).to.equal(0);
+
+      await hardhatToken.transfer(addr1.address, 50);
+      expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
+      expect(await hardhatToken.balanceOf(addr2.address)).to.equal(0);
+
+      await hardhatToken.connect(addr1).transfer(addr2.address, 50);
+      expect(await hardhatToken.balanceOf(addr1.address)).to.equal(0);
+      expect(await hardhatToken.balanceOf(addr2.address)).to.equal(50);
+    });
   });
 });
